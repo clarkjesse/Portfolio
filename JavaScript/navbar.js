4d3c8051ab0d89ea3ebf838fcb191a14672f94b3
clarkjesse <clark.jesse@gmail.com>
2017-11-30 00:08:57 -0600

    updated

--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -1,2 +1,9 @@
 // JavaScript source code
 
+var notifBannerClose = document.querySelector("#bannerClose");
+var notifBanner = document.querySelector(".alert");
+
+//Notification Banner Event Listeners
+notifBannerClose.addEventListener("click", function() {
+    
+});
